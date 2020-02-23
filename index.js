12e1c568fb2427fed34774dfbaf80e5f4ac188aa
Jason Etcovitch <jasonetco@github.com>
2020-02-23 12:28:59 -0500

    Refactor entrypoint

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 require('dotenv').config()
+const core = require('@actions/core')
 const { Toolkit } = require('actions-toolkit')
+const labelSponsor = require('./lib')
 
-Toolkit.run(require('./lib'), {
-  event: [
-    'pull_request.opened',
-    'issues.opened'
-  ],
-  secrets: [
-    'GITHUB_TOKEN'
-  ]
-})
+try {
+  const tools = new Toolkit({
+    event: [
+      'pull_request.opened',
+      'issues.opened'
+    ],
+    secrets: [
+      'GITHUB_TOKEN'
+    ]
+  })
+
+  await labelSponsor(tools)
+} catch (err) {
+  core.setFailed(err)
+  process.exit()
+}
