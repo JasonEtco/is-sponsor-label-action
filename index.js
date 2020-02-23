f14e26dc55edef9b6deb2c7ddc09efc678c50f67
Jason Etcovitch <jasonetco@github.com>
2020-02-23 13:27:58 -0500

    Fix erroring out

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 require('dotenv').config()
-const core = require('@actions/core')
 const { Toolkit } = require('actions-toolkit')
 const labelSponsor = require('./lib')
 
 async function run () {
-  try {
-    const tools = new Toolkit({
-      event: [
-        'pull_request.opened',
-        'issues.opened'
-      ],
-      secrets: [
-        'GITHUB_TOKEN'
-      ]
-    })
+  const tools = new Toolkit({
+    event: [
+      'pull_request.opened',
+      'issues.opened'
+    ],
+    secrets: [
+      'GITHUB_TOKEN'
+    ]
+  })
 
+  try {
     await labelSponsor(tools)
   } catch (err) {
-    core.setFailed(err)
-    process.exit()
+    tools.exit.failure(err)
   }
 }
 
