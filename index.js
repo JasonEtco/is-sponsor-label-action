c3efcb740d7bddab3b1aa42d4cf30d5e56b47fb5
Jason Etcovitch <jasonetco@github.com>
2020-02-23 13:26:27 -0500

    Fix async/await issue

--- index.js
+++ index.js
@@ -3,19 +3,23 @@ const core = require('@actions/core')
 const { Toolkit } = require('actions-toolkit')
 const labelSponsor = require('./lib')
 
-try {
-  const tools = new Toolkit({
-    event: [
-      'pull_request.opened',
-      'issues.opened'
-    ],
-    secrets: [
-      'GITHUB_TOKEN'
-    ]
-  })
+async function run () {
+  try {
+    const tools = new Toolkit({
+      event: [
+        'pull_request.opened',
+        'issues.opened'
+      ],
+      secrets: [
+        'GITHUB_TOKEN'
+      ]
+    })
 
-  await labelSponsor(tools)
-} catch (err) {
-  core.setFailed(err)
-  process.exit()
+    await labelSponsor(tools)
+  } catch (err) {
+    core.setFailed(err)
+    process.exit()
+  }
 }
+
+run()
