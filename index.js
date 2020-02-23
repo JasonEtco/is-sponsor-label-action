cf5f396d2871eef150de6e4e258c168900cb83fc
Jason Etcovitch <jasonetco@github.com>
2020-02-23 16:03:58 -0500

    Upgrade actions-toolkit

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 require('dotenv').config()
 const { Toolkit } = require('actions-toolkit')
-const labelSponsor = require('./lib')
+const getAuthorNodeId = require('./lib/get-author-id')
+const userIsSponsor = require('./lib/user-is-sponsor')
+const addLabel = require('./lib/add-label')
 
-async function run () {
-  const tools = new Toolkit({
-    event: [
-      'pull_request.opened',
-      'issues.opened'
-    ],
-    secrets: [
-      'GITHUB_TOKEN'
-    ]
-  })
+Toolkit.run(async tools => {
+  // Get the user id of the submitter
+  const nodeId = getAuthorNodeId(tools.context.payload)
 
-  try {
-    await labelSponsor(tools)
-  } catch (err) {
-    tools.exit.failure(err)
+  // Check if the user is a sponsor
+  const isSponsor = await userIsSponsor(tools, nodeId)
+  if (!isSponsor) {
+    tools.log.debug('Author is not a sponsor! Nothing left to do.')
+    return
   }
-}
 
-run()
+  // Add the label
+  await addLabel(tools)
+  tools.log.success('Label successfully applied. Have a nice day!')
+}, {
+  event: [
+    'pull_request.opened',
+    'issues.opened'
+  ],
+  secrets: [
+    'GITHUB_TOKEN'
+  ]
+})
