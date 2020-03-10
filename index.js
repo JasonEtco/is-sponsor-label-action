61e827a19c8a780fb1b31460db365e8b5526eb7b
Jason Etcovitch <jasonetco@github.com>
2020-03-10 13:51:39 -0400

    Use GITHUB_ACTOR

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 require('dotenv').config()
 const { Toolkit } = require('actions-toolkit')
-const getAuthorNodeId = require('./lib/get-author-id')
 const userIsSponsor = require('./lib/user-is-sponsor')
 const createLabel = require('./lib/create-label')
 const addLabel = require('./lib/add-label')
 
 Toolkit.run(async tools => {
-  // Get the login of the submitter
-  const possibleSponsor = getAuthorNodeId(tools.context.payload)
-
   // Check if the user is a sponsor
-  const isSponsor = await userIsSponsor(tools, possibleSponsor)
+  const isSponsor = await userIsSponsor(tools)
   if (!isSponsor) {
     tools.log.debug('Author is not a sponsor! Nothing left to do.')
     return
